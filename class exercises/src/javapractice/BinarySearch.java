d5a62cfc74bc2e2368c88b54f8ddcc59c1fbf8e6
Rauldev <Rauldb22@gmail.com>
2014-11-21 08:40:47 +0100

    binarysearch

--- class exercises/src/javapractice/BinarySearch.java
+++ class exercises/src/javapractice/BinarySearch.java
@@ -6,18 +6,44 @@ public class BinarySearch {
 		
 		int key=15;
 		if(search(key)==-1) System.out.println("The key was not found in the array");
-		System.out.println("La clave es "+search(key));
+		System.out.println("The key is "+search(key));
 		
 		
 		
 	}
 	
 	
+	
+	// Reescribir hallando el centro en cada iteracion hasta que se encuentre la clave
+	
+	/*public static int search(int key){
+		
+		int[] array=new int[100];
+		for(int i=0;i<array.length;i++) array[i]=i;
+		int initialindex=0;
+		int finalindex=array.length-1;
+		
+		while(initialindex<=finalindex){
+			
+			int center=(initialindex+finalindex)/2;
+			
+			
+		}
+		
+	}
+	
+	*/
+	
+	
+	// No es optimo, reduce los ciclos pero hay que calcular en cada iteracion el centro
+	// para que sea escalable cuando sea un array mas grande
+	
 	public static int search(int key){
 		
 		int[] array=new int[100];
 		for(int i=0;i<array.length;i++) array[i]=i;
 		int middle=array.length/2;
+		if (array[middle]==key) return array[middle];
 		int firstindex=0;
 		int secondindex=0;
 		if(key>middle){
@@ -35,5 +61,9 @@ public class BinarySearch {
 		
 		
 	}
+	
+	
 
 }
+
+
