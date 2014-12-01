5d72d0f1ebdd2ba0d645280642e011ac772a7a1b
Rauldb <Rauldb22@gmail.com>
2014-12-01 14:17:28 +0100

    Checkmagic and Magicsquare

--- class exercises/src/javapractice/Checkmagic.java
+++ class exercises/src/javapractice/Checkmagic.java
@@ -3,53 +3,60 @@ package javapractice;
 public class Checkmagic {
 
 	public static void main(String[] args) {
-		
-		int[][] matrix={{17,24,1,8,15},{23,5,7,14,16},{4,6,13,20,22},{10,12,19,21,3},{11,18,25,2,9}};
-		checkmagic(matrix);
+
+		int[][] magicsquare = Magicsquare.generate(5); // Dependant on Magicsquare class, will throw an error if you don't have it on the package.
+		int[][] nonmagicsquare = { { 17, 14, 1, 8, 15 }, { 23, 5, 7, 14, 16 },
+				{ 4, 6, 13, 20, 22 }, { 10, 45, 21, 94, 3 },
+				{ 11, 18, 25, 2, 9 } };
+		int[][] notasquare = new int[3][2];
+		System.out.println("A square generated using our own method, the result is : "+checkmagic(magicsquare));
+		System.out.println("A manual input square, change to test if needed, result : "+checkmagic(nonmagicsquare));
+		System.out.println("An array with diferent values for axis X and y, not square, result :"+checkmagic(notasquare));
 
 	}
 
-	
-	public static boolean checkmagic(int matrix[][]){
+	public static boolean checkmagic(int matrix[][]) {
+
+		int[] counters = new int[4];
+		boolean ismagic = true;
+		int sumcounter=0;
+		counters[0] = 0;
+		counters[1] = 0;
+		counters[2] = 0;
+		counters[3] = 0;		
+		int m = 0;
+		int o = 0;
+
+		if (matrix.length != matrix[0].length) return false; // return value directly to avoid exceptions due to out of bounds calls to an array, since this method only works on square matrixes.
 		
-		int contx=0;
-		int conty=0;
-		int contdiaga=0;
-		int contdiagb=0;
-		int j=0;
-		int m=0;
-		int o=matrix.length-1;
-		
-		if(matrix.length!=matrix[0].length)return false;
-		
-		for(int i=0;i<matrix.length;i++){
-				contx+=matrix[i][j];
-				conty+=matrix[j][i];
-				j++;
-			
+		for (int i = 0; i < matrix.length; i++) { // checks rows and columns
+					
+			counters[0] += matrix[i][0];
+			counters[1] += matrix[0][i];
+
 		}
-		
-		for(int l=0;l<matrix.length;l++){
-			contdiaga+=matrix[l][m];
+
+		for (int l = 0; l < matrix.length; l++) { // checks upper left diagonal
+			counters[2] += matrix[l][m];			
 			m++;
+			
+		}
+
+		for (int n =matrix.length-1;n>-1;n--) { // checks bottom left
+														// diagonal
+			counters[3] += matrix[n][o];			
+			o++;
+
 		}
 		
-		for(int n=matrix.length-1;n!=-1;n--){
-			contdiagb+=matrix[n][o];
-			o--;
-			
-			
-			if(contx!=conty)return false;			
-			
-			
-			
-			
+		for(int p=0;p<counters.length;p++){		// Sum counters	
+			sumcounter+=counters[p];			
 		}
-		System.out.println(contx);
-		System.out.println(conty);
-		System.out.println(contdiaga);
-		System.out.println(contdiagb);		
-		return false;
+		
+		if(sumcounter!=(counters[0]*counters.length)) ismagic=false; // Compare counters sum to the product of any counter by the lenght of the array to check if all of the counters are the same. 
+		
+		return ismagic;
+		
 	}
-	
+
 }
--- class exercises/src/javapractice/Magicsquare.java
+++ class exercises/src/javapractice/Magicsquare.java
@@ -1,53 +1,58 @@
 package javapractice;
 
 public class Magicsquare {
-	
-	public static void main(String[]  args){
-		
-		final int size=5;
+
+	public static void main(String[] args) {
+
+		final int size = 5;
 		generate(size);
-	
+
 	}
-	
-	
-	
-	public static void generate(int size){
-		
-		int[][] matrix=new int[size][size];
-		int x=matrix[0].length/2+matrix[0].length%2-1;
-		int cont=1;
-		int y=0;
-		
-		while(cont!=matrix.length*matrix.length+1){		
-		matrix[y][x]=cont;
-		if(cont%size==0){
-			y++;
-			if(y==matrix.length) y=0;
-		}
-		
-		else{
-			if(y==0)y=matrix.length-1;
-			else y--;
-			if(x==matrix[0].length-1)x=0;
-			else x++;			
-		}
-		cont++;
-	}
-		
-		for(int i=0;i<matrix.length;i++){
-			
-						
-			for(int j=0;j<matrix[0].length;j++){
-				System.out.print(matrix[i][j]+" ");				
+
+	public static int[][] generate(int size) {
+
+		int[][] matrix = new int[size][size];
+		int x = matrix[0].length / 2 + matrix[0].length % 2 - 1;
+		int cont = 1;
+		int y = 0;
+
+		while (cont != matrix.length * matrix.length + 1) {
+			matrix[y][x] = cont;
+			if (cont % size == 0) {
+				y++;
+				if (y == matrix.length)
+					y = 0;
 			}
-			
-			System.out.print("\n");
-			
+
+			else {
+				if (y == 0)
+					y = matrix.length - 1;
+				else
+					y--;
+				if (x == matrix[0].length - 1)
+					x = 0;
+				else
+					x++;
+			}
+			cont++;
 		}
-			
-			
+
+		for (int i = 0; i < matrix.length; i++) {
+
+			/*
+			 * testing loop
+			 * 
+			 * for(int j=0;j<matrix[0].length;j++){
+			 * System.out.print(matrix[i][j]+" "); }
+			 * 
+			 * System.out.print("\n");
+			 * 
+			 * testing loop end
+			 */
+
 		}
-		
+		return matrix;
+
 	}
 
-
+}
