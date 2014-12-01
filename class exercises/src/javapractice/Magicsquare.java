0d1f079e8c3f129106b6420fb56a15b98ec2d960
Rauldev <Rauldb22@gmail.com>
2014-12-01 11:44:11 +0100

    magicsquarefixed

--- class exercises/src/javapractice/Magicsquare.java
+++ class exercises/src/javapractice/Magicsquare.java
@@ -2,16 +2,52 @@ package javapractice;
 
 public class Magicsquare {
 	
-	public static int[][] generate(int matrix[][]){
+	public static void main(String[]  args){
 		
-		int start=matrix.length/2+matrix.length%2;
-		int cont=0;
-		while(cont!=matrix.length*matrix.length){		
+		final int size=5;
+		generate(size);
+	
+	}
+	
+	
+	
+	public static void generate(int size){
+		
+		int[][] matrix=new int[size][size];
+		int x=matrix[0].length/2+matrix[0].length%2-1;
+		int cont=1;
+		int y=0;
+		
+		while(cont!=matrix.length*matrix.length+1){		
+		matrix[y][x]=cont;
+		if(cont%size==0){
+			y++;
+			if(y==matrix.length) y=0;
+		}
+		
+		else{
+			if(y==0)y=matrix.length-1;
+			else y--;
+			if(x==matrix[0].length-1)x=0;
+			else x++;			
+		}
+		cont++;
+	}
+		
+		for(int i=0;i<matrix.length;i++){
 			
+						
+			for(int j=0;j<matrix[0].length;j++){
+				System.out.print(matrix[i][j]+" ");				
+			}
+			
+			System.out.print("\n");
+			
+		}
 			
 			
 		}
 		
 	}
 
-}
+
