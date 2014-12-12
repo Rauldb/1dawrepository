fd8d961cc966d23febf0603ae9dc7d7e0689fd92
Rauldb <Rauldb22@gmail.com>
2014-12-12 03:50:57 +0100

    polynomialsfinal

--- class exercises/src/proyecto1ertrimestre/Polynomials.java
+++ class exercises/src/proyecto1ertrimestre/Polynomials.java
@@ -1,66 +1,118 @@
 package proyecto1ertrimestre;
 
-import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.ArrayList;
-
-
 
 public class Polynomials {
 
 	public static void main(String[] args) {
 
-		// int[] testing = { 4, 0, 2, 0, 5 };
-		// System.out.println(display(testing));
-		
 		int option;
 		boolean polyinit = false;
-		ArrayList<int[]> list=new ArrayList<int[]>();
+		ArrayList<int[]> list = new ArrayList<int[]>();
 
 		do {
 			option = menu();
 			switch (option) {
 			case 1:
-				int[] buffer=enterpolynomial();
+				int[] buffer = enterpolynomial();
 				list.add(buffer);
 				polyinit = true;
 				break;
-
 			case 2:
 				if (!polyinit) {
 					System.out.println("You didn't enter any polynomials yet");
-					break;		
-					}
-				if(list.size()<2){
-					System.out.println("You need to introduce at least 2 polynomials so you can sum them");
 					break;
 				}
-				for(int i=0;i<list.size();i++){
-					int[] buffer2=(int[])list.get(i);
-					System.out.println((i+1)+" - "+display(buffer2));
-					}
+				System.out.println("Choose a polynomial to evaluate");
+				for (int i = 0; i < list.size(); i++) {
+					int[] buffer2 = (int[]) list.get(i);
+					System.out.println((i + 1) + " - " + display(buffer2));
+				}
+				int evaloption = -1;
+				do {
+					evaloption = integerinput();
+					if (evaloption < 0 || evaloption > list.size())
+						System.out
+								.println("That polinomy doesn't exist, please select one from the list");
+				} while (evaloption < 0 || evaloption > list.size());
+				System.out.println("Choose a value for X");
+				int xvalue = integerinput();
+				System.out.println(display(list.get(evaloption - 1)));
+				System.out.println("The Sum of the polinomy is "
+						+ evaluate(list.get(evaloption - 1), xvalue) + "\n");
+				break;
+
+			case 3:
+				if (!polyinit) {
+					System.out.println("You didn't enter any polynomials yet");
+					break;
+				}
+				if (list.size() < 2) {
+					System.out
+							.println("You need to introduce at least 2 polynomials so you can sum them");
+					break;
+				}
+				for (int i = 0; i < list.size(); i++) {
+					int[] buffer2 = (int[]) list.get(i);
+					System.out.println((i + 1) + " - " + display(buffer2));
+				}
+				int sumoption1 = -1;
+				int sumoption2 = -1;
 				System.out.println("Select 2 polynomials to sum");
 				System.out.println("Select polynomial 1");
-				int option1=integerinput();
+				while (sumoption1 < 0 || sumoption1 > list.size())
+					sumoption1 = integerinput();
 				System.out.println("Select polynomial 2");
-				int option2=integerinput();
-				int[] buffera=list.get(option1);
-				int[] bufferb=list.get(option2);
-				
-				
-				
-				break;
-			case 3:
+				while (sumoption2 < 0 || sumoption2 > list.size())
+					sumoption2 = integerinput();
+				int[] buffersuma = list.get(sumoption1 - 1);
+				int[] buffersumb = list.get(sumoption2 - 1);
+				System.out.println(display(buffersuma));
+				System.out.println(display(buffersumb));
+				System.out.println("----------------------------------------");
+				System.out.println(display(sum(buffersuma, buffersumb)) + "\n");
+
 				break;
 			case 4:
 				if (!polyinit) {
 					System.out.println("You didn't enter any polynomials yet");
-					break;		
-					}
-				for(int i=0;i<list.size();i++){
-					int[] buffer2=(int[])list.get(i);
-					System.out.println((i+1)+" - "+display(buffer2));
-					}								
+					break;
+				}
+				if (list.size() < 2) {
+					System.out
+							.println("You need to introduce at least 2 polynomials so you can sum them");
+					break;
+				}
+				for (int i = 0; i < list.size(); i++) {
+					int[] buffer2 = (int[]) list.get(i);
+					System.out.println((i + 1) + " - " + display(buffer2));
+				}
+				int multoption1 = -1;
+				int multoption2 = -1;
+				System.out.println("Select 2 polynomials to multiply");
+				System.out.println("Select polynomial 1");
+				while (multoption1 < 0 || multoption1 > list.size())
+					multoption1 = integerinput();
+				System.out.println("Select polynomial 2");
+				while (multoption2 < 0 || multoption2 > list.size())
+					multoption2 = integerinput();
+				int[] buffermulta = list.get(multoption1 - 1);
+				int[] buffermultb = list.get(multoption2 - 1);
+				System.out.println(display(buffermulta));
+				System.out.println(display(buffermultb));
+				System.out.println("----------------------------------------");
+				System.out.println(display(multiply(buffermulta, buffermultb)) + "\n");
+				break;
+			case 5:
+				if (!polyinit) {
+					System.out.println("You didn't enter any polynomials yet");
+					break;
+				}
+				for (int i = 0; i < list.size(); i++) {
+					int[] buffer2 = (int[]) list.get(i);
+					System.out.println((i + 1) + " - " + display(buffer2));
+				}
 				break;
 
 			}
@@ -69,40 +121,41 @@ public class Polynomials {
 
 	}
 
-	public static int menu() {
+	public static int menu() { // Menu method
 
-		Scanner input = new Scanner(System.in);
 		System.out.println("POLYNOMIALS");
 		System.out.println("-----------------------------");
 		System.out.println("1 - Enter a polynomial");
-		System.out.println("2 - Sum polynomials");
-		System.out.println("3 - Multiply polynomials");
-		System.out.println("4 - Display polynomial");
+		System.out.println("2 - Evaluate a polynomial");
+		System.out.println("3 - Sum polynomials");
+		System.out.println("4 - Multiply polynomials");
+		System.out.println("5 - Display polynomial");
 		System.out.println("0 - Exit ");
 
-		int option = input.nextInt();
+		int option = integerinput();
 		return option;
 	}
 
-	public static int[] enterpolynomial() {
+	public static int[] enterpolynomial() { // Prompts the user to enter a
+											// polynomial , grade by grade.
 
-		Scanner input = new Scanner(System.in);
 		int grade;
 		int monomial;
 		System.out.println("Enter the grade of the polynomial");
-		grade = input.nextInt();
-		int[] polynomial = new int[grade+1];
+		grade = integerinput();
+		int[] polynomial = new int[grade + 1];
 		for (int i = 0; i < polynomial.length; i++) {
-			System.out.println("Enter the grade " + i
-					+ " monomial of the polynomial");
-			monomial = input.nextInt();
+			System.out.println("Enter the coefficient of the grade " + i
+					+ " monomial (Enter a 0 to skip a grade) ");
+			monomial = integerinput();
 			polynomial[i] = monomial;
 		}
 		return polynomial;
 
 	}
 
-	public static int[] sum(int a[], int b[]) {
+	public static int[] sum(int a[], int b[]) { // This method sums 2
+												// polynomials
 
 		if (a.length > b.length) {
 			for (int i = 0; i < b.length; i++)
@@ -114,41 +167,71 @@ public class Polynomials {
 		return b;
 
 	}
+	
+	
+	public static int[] multiply(int a[], int[] b){
+		
+		int[] resultmult=new int[a.length*b.length];
+		
+		
+		for(int i=0;i<a.length;i++){
+			
+			if(a[i]!=0){
+				for(int j=0;j<b.length;j++){
+					
+				  if(b[j]!=0)resultmult[i+j]+=(a[i]*b[j]);
+				}
+			}
+			
+		}
+		return resultmult;
+	}
 
-	public static int evaluate(int[] polynomial, int xvalue) {
+	public static int evaluate(int[] polynomial, int xvalue) { // This method
+																// evaluates a
+																// polynomial
 
-		int result = 0;
+		int resulteva = 0;
 		for (int i = 0; i < polynomial.length; i++)
-			result += polynomial[i] * Math.pow(xvalue, i);
-		return result;
+			resulteva += polynomial[i] * Math.pow(xvalue, i);
+		return resulteva;
 
 	}
 
-	public static String display(int[] polynomial) {
+	public static String display(int[] polynomial) {	// This method displays a polynomial as closely as possible to how we write it on paper
 
 		String display = "";
 		for (int i = polynomial.length - 1; i >= 0; i--) {
 
 			if (polynomial[i] != 0) {
-				if (i == 0)
-					display += "+ " + polynomial[i];
-				else
-					display += " " + polynomial[i] + "X" + "^" + i + " ";
-			}
+				
+				if(polynomial[i]>=0){
+					if (i == 0)	display += "+" + polynomial[i];
+					else display +="+"+polynomial[i] + "X^" + i;
+					}
+				else{
+					if (i == 0)	display +=polynomial[i];
+					else display +=polynomial[i] + "X" + "^" + i;
+					}
 		}
+		display+=" ";
+			
+	   }
 		return display;
-
 	}
-	
-	public static int integerinput(){
-		Scanner input=new Scanner(System.in);
-		while(!input.hasNextInt() ) {
+
+	public static int integerinput() { // This method doesn't let you introduce
+										// anything that isn't a int value, it
+										// solves most user generated
+										// exceptions.
+		Scanner input = new Scanner(System.in);
+		while (!input.hasNextInt()) {
 			System.out.println("Enter an integer please");
 			input.next();
 		}
-		
+
 		return input.nextInt();
-		
+
 	}
 
 }
